1415f4d8b0cb07be8ffa4a871e9e1f895faf3216
tdbnz-coding <49893160+tdbnz-coding@users.noreply.github.com>
2024-11-16 22:19:27 +1300

    update

---
+++ streaming/streaminghub/nz/skysportsnz/script.js
@@ -0,0 +1,15 @@
+function changeStream(url) {
+    const streamFrame = document.getElementById('streamFrame');
+    streamFrame.src = url;
+}
+
+function toggleTheme() {
+    const body = document.body;
+    if (body.classList.contains('dark-theme')) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    }
+}
